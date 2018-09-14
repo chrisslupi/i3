e08dc9af14e48891546d8b21b082e92d18d16c2b
chrisslupi <gudmei@web.de>
2018-09-14 14:56:47 +0200

    Comment :  on Fri Sep 14 14:56:47 CEST 2018

--- git-v1.sh
+++ git-v1.sh
@@ -1,37 +1,10 @@
 #!/bin/bash
 #
 ##################################################################################################################
-# Written to be used on 64 bits computers
-# Author 	: 	Erik Dubois
-# Website 	: 	http://www.erikdubois.be
-##################################################################################################################
-##################################################################################################################
-#
-#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
-#
+
 ##################################################################################################################
 
-# echo "# Ultimate-Linux-Mint-18" >> README.md
-# git init
-# git add README.md
-# git commit -m "first commit"
-# git remote add origin https://github.com/erikdubois/Ultimate-Linux-Mint-18-Cinnamon.git
-# git push -u origin master
 
-# git config --global user.name x
-# git config --global user.email x
-# sudo git config --system core.editor nano
-# git config --global credential.helper cache
-# git config --global credential.helper 'cache --timeout=3600'
-
-
-# Force git to overwrite local files on pull - no merge
-
-# git fetch all
-
-# git push --set-upstream origin master
-
-# git reset --hard orgin/master
 
 
 # checking if kernel files are present otherswise github will become too big
--- installation/400-install-software-distro-specific-v1.sh
+++ installation/400-install-software-distro-specific-v1.sh
@@ -1,28 +1,58 @@
 #!/bin/bash
-set -e
+###################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
 ##################################################################################################################
 
-##################################################################################################################
-
-
-#Core stuff i3
-
 echo "################################################################"
-echo "i 3  improved tiling core files"
+echo "#########   distro specific software installed  ################"
 echo "################################################################"
 
-sudo pacman -S --noconfirm --needed i3lock i3status
+#Fonts
+
+## sudo pacman -S ttf-ubuntu-font-family --noconfirm --needed conflicts with ttf-google-fonts
+## sudo pacman -S ttf-droid --noconfirm --noconfirm --needed  conflicts with ttf-google-fonts
+## sudo pacman -S ttf-inconsolata --noconfirm --needed        conflicts with ttf-google-fonts
+sudo pacman -S noto-fonts --noconfirm --needed
+
+#file manager
+
+sudo pacman -S nemo --noconfirm --needed
+
+
+#Fonts
+
+sudo pacman -S ttf-ubuntu-font-family --noconfirm --needed
+sudo pacman -S ttf-droid --noconfirm --noconfirm --needed
+sudo pacman -S ttf-inconsolata --noconfirm --needed
+
+#Utilities
+sudo pacman -S pamac --noconfirm --needed 
+sudo pacman -S notify-osd  --noconfirm --needed
+sudo pacman -S lxappearance --noconfirm --needed
+sudo pacman -S feh --noconfirm --needed
+sudo pacman -S arandr --noconfirm --needed
+sudo pacman -S qt4 --confirm --needed
+sudo pacman -S xorg-xrandr --noconfirm --needed
+sudo pacman -S gvfs  --noconfirm --needed
+sudo pacman -S compton  --noconfirm --needed
+sudo pacman -S volumeicon  --noconfirm --needed
+#sudo yay -S arandr  --noconfirm
+#run with systemadm
+#sudo yay -S systemd-ui  --noconfirm
 
 
 
+        # S Y S T E M  T O O L S # 
 
-# gnome
+# steering spotify from the keyboard G15 Logitech - volume and next/previous/stop/pause
 
 echo "################################################################"
-echo "j4-dmenu-desktop"
+echo "playerctl"
 echo "################################################################"
 
-package="j4-dmenu-desktop"
+package="playerctl"
 
 #----------------------------------------------------------------------------------
 
@@ -42,117 +72,20 @@ else
 		yay -S --noconfirm --noedit  $package
 
 	elif pacman -Qi pacaur &> /dev/null; then
-
+		
 		echo "Installing with pacaur"
 		pacaur -S --noconfirm --noedit  $package
-
+		 	
 	elif pacman -Qi yaourt &> /dev/null; then
 
 		echo "Installing with yaourt"
 		yaourt -S --noconfirm $package
-
-	fi
-
-
-fi
-
-
-
-
-
-
-echo "################################################################"
-echo "i3blocks"
-echo "################################################################"
-
-package="i3blocks"
-
-#----------------------------------------------------------------------------------
-
-#checking if application is already installed or else install with aur helpers
-if pacman -Qi $package &> /dev/null; then
-
-	echo "################################################################"
-	echo "################## "$package" is already installed"
-	echo "################################################################"
-
-else
-
-	#checking which helper is installed
-	if pacman -Qi yay &> /dev/null; then
-
-		echo "Installing with yay"
-		yay -S --noconfirm --noedit  $package
-
-	elif pacman -Qi pacaur &> /dev/null; then
-
-		echo "Installing with pacaur"
-		pacaur -S --noconfirm --noedit  $package
-
-	elif pacman -Qi yaourt &> /dev/null; then
-
-		echo "Installing with yaourt"
-		yaourt -S --noconfirm $package
-
+			  	
 	fi
 
 	# Just checking if installation was successful
 	if pacman -Qi $package &> /dev/null; then
-
-	echo "################################################################"
-	echo "#########  "$package" has been installed"
-	echo "################################################################"
-
-	else
-
-	echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-	echo "!!!!!!!!!  "$package" has NOT been installed"
-	echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-
-	fi
-
-fi
-
-
-
-echo "################################################################"
-echo "i3-gaps-next-git"
-echo "################################################################"
-
-package="i3-gaps-next-git"
-
-#----------------------------------------------------------------------------------
-
-#checking if application is already installed or else install with aur helpers
-if pacman -Qi $package &> /dev/null; then
-
-	echo "################################################################"
-	echo "################## "$package" is already installed"
-	echo "################################################################"
-
-else
-
-	#checking which helper is installed
-	if pacman -Qi yay &> /dev/null; then
-
-		echo "Installing with yay"
-		yay -S --noconfirm --noedit  $package
-
-	elif pacman -Qi pacaur &> /dev/null; then
-
-		echo "Installing with pacaur"
-		pacaur -S --noconfirm --noedit  $package
-
-	elif pacman -Qi yaourt &> /dev/null; then
-
-		echo "Installing with yaourt"
-		yaourt -S --noconfirm $package
-
-	fi
-
-	# Just checking if installation was successful
-	if pacman -Qi $package &> /dev/null; then
-
+	
 	echo "################################################################"
 	echo "#########  "$package" has been installed"
 	echo "################################################################"
@@ -172,5 +105,70 @@ fi
 
 
 echo "################################################################"
-echo "###################    i3 core installed  ######################"
+echo "trayicon for sound for bluetooth headphone"
+echo "################################################################"
+
+
+package="pasystray-git"
+
+#----------------------------------------------------------------------------------
+
+#checking if application is already installed or else install with aur helpers
+if pacman -Qi $package &> /dev/null; then
+
+	echo "################################################################"
+	echo "################## "$package" is already installed"
+	echo "################################################################"
+
+else
+
+	#checking which helper is installed
+	if pacman -Qi yay &> /dev/null; then
+
+		echo "Installing with yay"
+		yay -S --noconfirm --noedit  $package
+
+	elif pacman -Qi pacaur &> /dev/null; then
+		
+		echo "Installing with pacaur"
+		pacaur -S --noconfirm --noedit  $package
+		 	
+	elif pacman -Qi yaourt &> /dev/null; then
+
+		echo "Installing with yaourt"
+		yaourt -S --noconfirm $package
+			  	
+	fi
+
+	# Just checking if installation was successful
+	if pacman -Qi $package &> /dev/null; then
+	
+	echo "################################################################"
+	echo "#########  "$package" has been installed"
+	echo "################################################################"
+
+	else
+
+	echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+	echo "!!!!!!!!!  "$package" has NOT been installed"
+	echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+
+	echo "To install pasytray, go to tmp file and run from there"
+	echo "sudo pacman -U /tmp/yaybuild-1000/pasystray-git/pasystray-git/pasystray-git-0.6.0.r13.gccb5b6e-1-x86_64.pkg.tar.xz"
+	echo "Trying to run the git version"
+	sudo pacman -U /tmp/yaybuild-1000/pasystray-git/pasystray-git/pasystray-git*  --needed --noconfirm
+	fi
+
+fi
+
+
+
+
+###############################################################
+
+##############################################
+###############################################################################################
+
+echo "################################################################"
+echo "#########   distro specific software installed  ################"
 echo "################################################################"
