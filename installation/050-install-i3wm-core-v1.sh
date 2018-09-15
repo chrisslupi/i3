561d8e6021b68d2cacf9e435557c21851c701d0d
chrisslupi <gudmei@web.de>
2018-09-15 14:21:09 +0200

    Comment :  on Sat Sep 15 14:21:09 CEST 2018

--- installation/050-install-i3wm-core-v1.sh
+++ installation/050-install-i3wm-core-v1.sh
@@ -82,7 +82,7 @@ else
 	if pacman -Qi yay &> /dev/null; then
 
 		echo "Installing with yay"
-		yay -S --noconfirm --$package
+		yay -S --noconfirm  $package
 
 	elif pacman -Qi pacaur &> /dev/null; then
 
